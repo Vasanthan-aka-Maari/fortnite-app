797eb31a06e0226b43fe737ff0e9003bbfb83c9a
Vasanthan <vasanth2754@gmail.com>
2020-10-11 19:44:18 +0530

    Updates

---
+++ precache-manifest.73a28caceb593d1e1dfbdba9fbc3af5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4295b9acf27af3bdbd1852ad47a6e3a",
+    "url": "/fortnite-app/index.html"
+  },
+  {
+    "revision": "8e524a39321ccd53608a",
+    "url": "/fortnite-app/static/css/main.0a74b1da.chunk.css"
+  },
+  {
+    "revision": "653ea3d277427cf8e77b",
+    "url": "/fortnite-app/static/js/2.21c59bb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fortnite-app/static/js/2.21c59bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e524a39321ccd53608a",
+    "url": "/fortnite-app/static/js/main.20ca410b.chunk.js"
+  },
+  {
+    "revision": "8488773881f5c69b99e9",
+    "url": "/fortnite-app/static/js/runtime-main.8fa9a906.js"
+  }
+]);
