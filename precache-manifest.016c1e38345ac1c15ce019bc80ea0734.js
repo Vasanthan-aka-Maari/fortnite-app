fcdd96511db01a87185ddb757d68c60fc00b28b9
Vasanthan <vasanth2754@gmail.com>
2020-10-29 23:20:20 +0530

    Updates

--- precache-manifest.016c1e38345ac1c15ce019bc80ea0734.js
+++ precache-manifest.016c1e38345ac1c15ce019bc80ea0734.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4295b9acf27af3bdbd1852ad47a6e3a",
+    "revision": "4607a304e33119ada378d65b304eea91",
     "url": "/fortnite-app/index.html"
   },
   {
-    "revision": "8e524a39321ccd53608a",
+    "revision": "be58c2f0d25de7c43165",
     "url": "/fortnite-app/static/css/main.0a74b1da.chunk.css"
   },
   {
-    "revision": "653ea3d277427cf8e77b",
-    "url": "/fortnite-app/static/js/2.21c59bb5.chunk.js"
+    "revision": "a4ffe59b4450c32c98d2",
+    "url": "/fortnite-app/static/js/2.ba1070e7.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/fortnite-app/static/js/2.21c59bb5.chunk.js.LICENSE.txt"
+    "url": "/fortnite-app/static/js/2.ba1070e7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8e524a39321ccd53608a",
-    "url": "/fortnite-app/static/js/main.20ca410b.chunk.js"
+    "revision": "be58c2f0d25de7c43165",
+    "url": "/fortnite-app/static/js/main.82be07c7.chunk.js"
   },
   {
     "revision": "8488773881f5c69b99e9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fortnite-app/precache-manifest.73a28caceb593d1e1dfbdba9fbc3af5f.js"
+  "/fortnite-app/precache-manifest.016c1e38345ac1c15ce019bc80ea0734.js"
 );
 
 self.addEventListener('message', (event) => {
